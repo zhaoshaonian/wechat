0e2a12032acf035ea2cc7f5207c78353cde33528
zhao de <535900639@qq.com>
2018-11-02 15:52:29 +0800

    链接更改

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,10 +4,13 @@ var data = require('../../utils/data.js').songs;
 Page({
   data: {
     imgUrls: [
-      '../../image/banner/banner1.jpg',
-      '../../image/banner/banner2.jpg',
-      '../../image/banner/banner3.jpg',
-      '../../image/banner/banner4.jpg'
+      'http://p1.music.126.net/Bn16ByIEjOyUCZsJ1R_dXw==/109951163638462544.jpg',
+      'http://p1.music.126.net/Jmrpx8nMfyJhvojTxoHxqQ==/109951163638454760.jpg',
+      'http://p1.music.126.net/N9ljku9Ltmj_Hf3zVe9nwA==/109951163638475643.jpg',
+      'http://p1.music.126.net/l00RpRax7_rx3TfEHXwp0g==/109951163638435877.jpg',
+      'http://p1.music.126.net/dHCZBMiOE-Hacku_eMj_7w==/109951163636707868.jpg',
+      'http://p1.music.126.net/viMu6v3-TJ_VSIirgTywrg==/109951163638548359.jpg',
+      'http://p1.music.126.net/bMGpdXxfBg-ie07AEyJSpg==/109951163636664500.jpg'
       
     ]
   },
